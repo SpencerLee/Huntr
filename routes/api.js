130b0d32201a60504ae352789633b2ebe5942efa
spencerlee <spencerlee1990@gmail.com>
2016-02-27 16:31:53 -0800

    creating api and html routing structure

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/users', function(req, res, next) {
+  res.send( {name: "Spencer", email: "spencerlee1990@gmail.com"});
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET users listing. */
+router.get('/login', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
 module.exports = router;
